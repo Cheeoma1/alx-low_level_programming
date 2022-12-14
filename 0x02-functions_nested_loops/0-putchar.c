13dfb42b2c6b3c80b0be6306c4c23428f817a564
cheeoma1 <jasycynthia124@gmail.com>
2022-12-14 04:55:07 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+able File  19 lines (19 sloc)  264 Bytes
+
+#include "main.h"
+/**
+ *  main - Prints _putchar
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
